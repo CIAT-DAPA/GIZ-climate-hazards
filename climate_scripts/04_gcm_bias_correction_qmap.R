9e251bd4cd0cf1199f0d57a351793f34a19195ab
haachicanoy <harold22010@gmail.com>
2020-05-02 20:53:27 -0500

    Updating running order

--- climate_scripts/04_gcm_bias_correction_qmap.R
+++ climate_scripts/04_gcm_bias_correction_qmap.R
@@ -157,6 +157,6 @@ rcpList    <- 'rcp85'
 gcmList <- c("ipsl_cm5a_mr","miroc_esm_chem","ncc_noresm1_m")
 for(p in periodList){
   for(gcm in gcmList){
-    BC_Qmap(country='Mali',county='Segou',rcp='rcp85',gcm=gcm,period=p,ncores=10)
+    BC_Qmap(country='Mali',county='Mopti',rcp='rcp85',gcm=gcm,period=p,ncores=10)
   }
 }
