8c0294d0f6ae68106dbdba285bca383ec6ebbedb
haachicanoy <harold22010@gmail.com>
2020-05-18 07:58:59 -0500

    Scripts 01 update

--- climate_scripts/02_loading_obs_climate.R
+++ climate_scripts/02_loading_obs_climate.R
@@ -121,4 +121,4 @@ get_observational_data <- function(country = 'Pakistan',
   
 }
 # Run twice
-for(i in 1:2){get_observational_data(country='Burkina_Faso',county='Haut-Bassins',iso3='BFA',adm_lvl=1)}
+for(i in 1:2){get_observational_data(country='Ivory_Coast',county='Comoe',iso3='CIV',adm_lvl=1)}
