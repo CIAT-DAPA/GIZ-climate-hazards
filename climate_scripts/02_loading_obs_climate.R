c6a2c1895dacd17142c94462bd9a8a10e20ffe3e
haachicanoy <harold22010@gmail.com>
2020-09-25 08:48:08 -0500

    Unsaved changes

--- climate_scripts/02_loading_obs_climate.R
+++ climate_scripts/02_loading_obs_climate.R
@@ -125,4 +125,4 @@ get_observational_data <- function(country = 'Pakistan',
   
 }
 # Run twice
-for(i in 1:2){get_observational_data(country='Kenya',county='Siaya',iso3='KEN',adm_lvl=1)}
+for(i in 1:2){get_observational_data(country='Nigeria',county='Bayelsa ',iso3='NGA',adm_lvl=1)}
